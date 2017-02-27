2eb8612a10a3d384dab3d2a24adec4152c959fc2
Joseph Burnett <josephburnett79@gmail.com>
2017-02-27 04:13:25 +0000

    Trie in separate cljc file.

--- src/mash_commander/generate.clj
+++ src/mash_commander/generate.clj
@@ -1,18 +1,13 @@
 (ns mash-commander.generate
-  (:require [clojure.data.json :as json]
-            [clojure.java.io :as io]
-            [clojure.string :as str]))
-
-(defn add-word [trie word]
-  (let [letters (into [] (map str/lower-case (str/split word #"")))]
-    (if (or (empty? letters) (= "#" (first letters)))
-      trie (assoc-in trie (conj letters "") ""))))
+  (:require [mash-commander.trie :as trie]
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]))
 
 (defn -main [_ root]
   (println "Generating word trie in project root:" root)
   (with-open [rdr (io/reader (str root "/words.txt"))]
-    (let [trie (reduce add-word {} (line-seq rdr))]
+    (let [t (trie/build (line-seq rdr))]
       (with-open [wrtr (io/writer (str root "/resources/public/words.json"))]
-        (.write wrtr (json/write-str trie)))))
+        (.write wrtr (json/write-str t)))))
   (println "Done."))
   
---
+++ src/mash_commander/trie.cljc
@@ -0,0 +1,10 @@
+(ns mash-commander.trie
+  (:require [clojure.string :as str]))
+
+(defn- add-word [trie word]
+  (let [letters (into [] (map str/lower-case (str/split word #"")))]
+    (if (or (empty? letters) (= "#" (first letters)))
+      trie (assoc-in trie (conj letters "") ""))))
+
+(defn build [words]
+  (reduce add-word {} words))
