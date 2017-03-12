b3b51a2d21c903afe946cb472a23c51c4ade76a2
Joseph Burnett <josephburnett79@gmail.com>
2017-03-12 14:35:13 -0700

    Deploy script.

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z ${PROJECT+x} ]; then
+    echo "Set PROJECT to deploy."
+    exit 1
+fi
+   
+export ROOT=`git rev-parse --show-toplevel`
+
+cd ${ROOT?}
+lein clean
+lein cljsbuild once min
+
+cd ${ROOT?}/gae
+gcloud --quiet --project ${PROJECT?} app deploy --no-promote
