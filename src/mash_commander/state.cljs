cd4613ad215016b8078b8fb0b6d59bdcb630970d
Joseph Burnett <josephburnett79@gmail.com>
2017-11-21 22:04:10 -0800

    Set URL parameter.

--- src/mash_commander/state.cljs
+++ src/mash_commander/state.cljs
@@ -1,5 +1,7 @@
 (ns mash-commander.state
   (:require [mash-commander.mode :as mode]
+            [mash-commander.set.manifest :as set-manifest]
+            [clojure.string :as string]
             [om.core :as om :include-macros true]))
 
 (defonce app-state
@@ -15,7 +17,21 @@
 (defn load []
   (print "State loaded."))
 
+; https://gist.github.com/kordano/56a16e1b28d706557f54
+(defn url-params []
+  (let [param-strs (-> (.-location js/window) (string/split #"\?") last (string/split #"\&"))]
+    (into {} (for [[k v] (map #(string/split % #"=") param-strs)]
+               [(keyword k) v]))))
+
 (defn init []
-  (swap! app-state #(assoc-in % [:lines :active]
-                              (mode/initial-line-state {:mode :freestyle}))))
-    
+  (let [set (:set (url-params))]
+    (print "set: " set)
+    (if (contains? @set-manifest/sets set)
+      (swap! app-state #(assoc-in % [:lines]
+                                  {:active (mode/initial-line-state {:mode :set
+                                                                     :set set})
+                                   :prev-active (mode/initial-line-state {:mode :freestyle})
+                                   :prev-history []}))
+      (swap! app-state #(assoc-in % [:lines :active]
+                                  (mode/initial-line-state {:mode :freestyle}))))))
+
