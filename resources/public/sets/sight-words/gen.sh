63a7a841ce193c5b7851a3fbc22550f7f3c9eac0
Joseph Burnett <josephburnett79@gmail.com>
2017-11-22 06:35:02 -0800

    Add missing i.png.

--- resources/public/sets/sight-words/gen.sh
+++ resources/public/sets/sight-words/gen.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# convert \
-#     -size 500x300 canvas:white \
-#     -gravity Center \
-#     -font NewCenturySchlbk-Roman \
-#     -pointsize 100 \
-#     -kerning 2 \
-#     -fill "#333" \
-#     -annotate +0+5 "$1" \
-#     -fill none \
-#     -stroke "#c3b7aa" \
-#     -strokewidth 1 \
-#     -draw "rectangle 0,0 499,299" \
-#     $1.png
+convert \
+    -size 500x300 canvas:white \
+    -gravity Center \
+    -font NewCenturySchlbk-Roman \
+    -pointsize 100 \
+    -kerning 2 \
+    -fill "#333" \
+    -annotate +0+5 "$1" \
+    -fill none \
+    -stroke "#c3b7aa" \
+    -strokewidth 1 \
+    -draw "rectangle 0,0 499,299" \
+    $1.png
 
 echo \
 "    {
