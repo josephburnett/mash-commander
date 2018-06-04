351a39d48bbfe5ef2b47ddbed3c601a9a5f40595
Joseph Burnett <josephburnett79@gmail.com>
2018-06-04 16:57:57 +0000

    Size keyboard relative to the minimum of window height and width.

--- src/mash_commander/keyboard.cljs
+++ src/mash_commander/keyboard.cljs
@@ -9,17 +9,17 @@
         potential (mode/key-potential line-cursor key)]
     (dom/div #js {:style (clj->js
                           (merge
-                           {:height "4.5vw"
-                            :width "5vw"
-                            :padding "1.2vw 0 0 0"
+                           {:height "4.5vmin"
+                            :width "5vmin"
+                            :padding "1.2vmin 0 0 0"
                             :backgroundColor "#222"
                             :float "left"
                             :textAlign "center"
-                            :fontSize "3vw"
-                            :borderWidth "0.3vw"
+                            :fontSize "3vmin"
+                            :borderWidth "0.3vmin"
                             :borderColor "#000"
                             :borderStyle "solid"
-                            :borderRadius "1.2vw"
+                            :borderRadius "1.2vmin"
                             :color (condp = potential
                                      :command "#33f"
                                       :typing "#0b0"
@@ -51,22 +51,22 @@
       (let [rv (partial row-view (:active cursor) owner)]
         (dom/div #js {:style #js {:position "absolute"
                                   :bottom "7vh"
-                                  :width "100vw"
+                                  :width "100vmin"
                                   :heigth "100vh"}}
                  (dom/div #js {:style #js {:backgroundColor "#ddd"
-                                           :width "90vw"
+                                           :width "90vmin"
                                            :height "25vm"
                                            :maxHeight "100%"
                                            :margin "0 auto"
-                                           :padding "0 0 0 3vw"
+                                           :padding "0 0 0 3vmin"
                                            :zIndex "100"}}
                           (rv (concat
-                               [{:key "Escape" :display "Esc" :style {:width "8vw" :marginRight "2vw"}}]
+                               [{:key "Escape" :display "Esc" :style {:width "8vmin" :marginRight "2vmin"}}]
                                (standard-keys "1234567890-")
-                               [{:key "Backspace" :display "<--" :style {:width "10vw" :marginLeft "7vw"}}]) "0vw")
-                          (rv (standard-keys "qwertyuiop") "13vw")
+                               [{:key "Backspace" :display "<--" :style {:width "10vmin" :marginLeft "7vmin"}}]) "0vmin")
+                          (rv (standard-keys "qwertyuiop") "13vmin")
                           (rv (concat
                                (standard-keys "asdfghjkl'")
-                               [{:key "Enter" :display "Enter":style {:width "15vw" :marginLeft "3vw"}}]) "15vw")
-                          (rv (standard-keys "zxcvbnm") "18vw")
-                          (rv [{:key " " :display "Space" :style {:width "27vw"}}] "29vw")))))))
+                               [{:key "Enter" :display "Enter":style {:width "15vmin" :marginLeft "3vmin"}}]) "15vmin")
+                          (rv (standard-keys "zxcvbnm") "18vmin")
+                          (rv [{:key " " :display "Space" :style {:width "27vmin"}}] "29vmin")))))))
